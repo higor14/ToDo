26b1d2ed36b6763de9856185572706a0d3f4750a
CodeHardX <washingtonjjunior@edu.pe.senac.br>
2025-11-24 11:43:04 -0300

    continuação
    criação de links

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from main.views import TaskList
+from . import views
 
 urlpatterns = [
     path("", TaskList.as_view(), name='task_list'),
+    path('funcao/', views.task_list, name='task_list_funcao'),
+    path('concluido/', views.task_concluido, name='task_concluido'),
+    path('nao concluidas', views.task_nao_concluido, name='task_nao_concluida')
 ]
--- main/views.py
+++ main/views.py
@@ -11,3 +11,32 @@ class TaskList(ListView):
     context_object_name = 'tarefas'
     
     # views - urls - template
+    
+def task_list(request):
+    tarefas = Task.objects.all()    #trazendo tudo para a tabela task
+    context = {
+        "tarefas": tarefas,
+        "titulo_pagina": 'Minhas Tarefas'
+    }
+    return render(request, 'tasks/task_list.html', context)
+
+def task_concluido(request):
+    a = 4
+    b = 5
+    c = a + b
+
+    tarefas = Task.objects.filter(concluida=1)  # trazendo tudo áta tabela task
+    context = {
+        "tarefas": tarefas,
+        "titulo_pagina": 'Minhas tarefas concluidas'
+    }
+    return render(request, 'tasks/teste.html', context)
+
+def task_nao_concluido(request):
+    
+    tarefas = Task.objects.filter(concluida=0)  # trazendo tudo áta tabela task
+    context = {
+        "tarefas": tarefas,
+        "titulo_pagina": 'Minhas tarefas nao concluidas'
+    }
+    return render(request, 'tasks/teste.html', context)
